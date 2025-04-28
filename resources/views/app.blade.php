53a9171535b060d5f392055348eb6ec00de04316
Ooops <dev.seb.3745@gmail.com>
2025-04-28 18:45:15 -0500

    ziggy errors

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes
+        @routes()
         @vite(['resources/js/app.ts'])
         @inertiaHead
     </head>
