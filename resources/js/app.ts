9ef84ae7c0db8fa66ac64bd44ee6a82b445c311f
Ooops <dev.seb.3745@gmail.com>
2025-04-28 18:51:12 -0500

    ziggy dist files added

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,10 @@
 import '../css/app.css';
 
+import { ZiggyVue } from '@/../../utils/ziggy';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
 
 // Extend ImportMeta interface for Vite...
